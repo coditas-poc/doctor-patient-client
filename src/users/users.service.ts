a56add846f1a1b7ba6de4a143d5a8e53f9b46cd7
Claudia <claudia.alphonse@coditas.com>
2020-01-17 16:57:23 +0530

    added user apis

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,4 +30,10 @@ export class UsersService {
                 throw new HttpException(error, error.status);
             });
     }
+    async verifyUserOtp(details) {
+        this.logger.log('Fectching patient Details');
+        return this.client.send('verifyOtp', details).toPromise().catch(error => {
+            throw new HttpException(error, error.status);
+        });
+    }
 }
